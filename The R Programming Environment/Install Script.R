e0105e49e89ec61d8c75b0929eb5a810ee0a7b53
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2024-07-28 20:37:23 +0800

    Add files via upload

---
+++ The R Programming Environment/Install Script.R
@@ -0,0 +1,48 @@
+install.packages(c('readr', 'ggplot2', 'tidyr'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+
+install.packages(c('dslabs', 'dpylr', 'tibbles'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+
+install.packages(c('swirl'), 
+contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+install.packages(c('base64enc', 'highr', 'htmltools', 'knitr', 'markdown', 'rmarkdown', 'xfun'), 
+contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+install.packages(c('lubridate'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+install.packages(c('caret'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+install.packages(c('learnr'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+
+install.packages(c('rmarkdown'), 
+                 contriburl="https://cran.r-project.org/bin/windows/contrib/r-release/")
+
+install.packages(c('rlang'), 
+                 contriburl="https://cran.r-project.org/bin/windows/contrib/r-release/")
+
+
+install.packages(c('MASS'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+
+install.packages(c('tidyverse'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+
+install.packages(c('pdftools'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+
+install.packages(c('microbenchmark'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
+
+install.packages(c('faraway','titanic'), 
+                 contriburl="https://cran-archive.r-project.org/bin/windows/contrib/3.6/")
